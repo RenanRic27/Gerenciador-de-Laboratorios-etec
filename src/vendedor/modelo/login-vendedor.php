3d4c791ea0dceb7de5edf74f4c63240c1137b605
RenanRic27 <renanricardo2781@gmail.com>
2022-08-16 10:55:11 -0300

    commit

---
+++ src/vendedor/modelo/login-vendedor.php
@@ -0,0 +1,25 @@
+<?php
+
+//consulta ao banco de dados
+include('../../conexao/conn.php');
+
+ // SQL de consulta dos dados digitados
+ $sql = $pdo ->query("SELECT *, count(ID) as achou FROM vendedor WHERE LOGIN = '".$_REQUEST['LOGIN']."' AND SENHA = '".md5($_REQUEST['SENHA'])."'");
+
+while($resultado = $sql ->fetch(PDO::FETCH_ASSOC)){
+    if($resultado['achou'] == 1){
+         $_SESSION['NOME'] = $resultado['NOME'];
+         $_SESSION['TIPO'] = $resultado['TIPO_ID'];
+         $dados = array(
+            'tipo' => 'success',
+            'mensagem' => 'login correto'
+
+         );
+    }else{
+        $dados = array(
+            'tipo' => 'error',
+            'mensagem' => 'Login e/ou senha incorreta'
+        );
+    }
+}
+echo json_encode($dados);
