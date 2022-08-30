8e1ff4e57fb04c556fb76a8f80613f318d4f0aa8
RenanRic27 <renanricardo2781@gmail.com>
2022-08-30 10:51:00 -0300

    commit

---
+++ src/promocao/controle/new-promocao.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+    $('.btn-new').click(function(e) {
+        e.preventDefault()
+
+        $('.modal-title').empty()
+
+
+        $('.modal-title').append('Adicionar novo promoção')
+
+        $('.modal-body').load('src/promocao/visao/form-promocao.html')
+
+        $('.btn-save').show()
+
+        $('.btn-save').attr('data-operation', 'insert')
+
+        $('#modal-promocao').modal('show')
+    })
+
+    $('.close, #close').click(function(e) {
+        e.preventDefault()
+        $('#modal-promocao').modal('hide')
+    })
+})
